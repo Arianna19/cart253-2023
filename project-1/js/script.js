9c6a1c6bc893d74ad36af5e38f12615e35cc5786
Arianna19 <ariannayurie@gmail.com>
2023-10-10 17:39:20 -0400

    Creating the functions and screen changes depending on ending

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -9,9 +9,7 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
+
 function preload() {
 
 }
@@ -21,6 +19,8 @@ function preload() {
  * Description of setup
 */
 function setup() {
+    createCanvas(500,500);
+    
 
 }
 
@@ -29,5 +29,79 @@ function setup() {
  * Description of draw()
 */
 function draw() {
+
+    if (state === `start`) { //clicking space to start 
+        start();
+    }
+    //Kirby dodging and collecting candies (main part)
+    else if (state === `simulation`) { 
+        simulation();
+    }
+    //good ending when user collects 31 candies 
+    else if (state === `happy halloween`) { 
+        happyHalloween();
+    }
+
+    //bad ending of the game where user interacts with over 3 bad elements
+    else if (state === `kirby tricked`) {
+        kirbyTricked();
+    }
+}
+
+function start() {
+    //start screen/main menu 
+
+}
+
+function simulation() {
+    //where user is dodging bad elements and collecting candy
+
+}
+
+function happyHalloween() {
+    //good ending screen
+
+}
+
+function kirbyTricked() {
+    //bad ending screen
+
+}
+
+function bigCandy() {
+    //when interacted with kirby +5
+
+}
+
+function smallCandy() {
+    //when interacted with kirby +3 points
+
+}
+
+function bat() {
+    //when interacted with kirby -1 life (bad element)
+
+}
+
+function ghosts() {
+    //when interacted with kirby -1 life (bad element)
+
+}
+
+function kirby() {
+
+}
+
+function restartGame() {
+
+}
+
+function candyCounter() {
+    //keeps track of the amount of candies collected by user
+
+}
+
+function kirbyLives() {
+    //user gets 3 lives at the beginning of game
 
 }
