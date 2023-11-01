692356cd9b075471a7de129b835ea12c09cc9a75
Arianna <70153178+Arianna19@users.noreply.github.com>
2023-11-01 19:38:34 -0400

    Making the user a watering kirby and creating a new object using an image for the butterflies. Also creating a new class for Kirby(user)

---
+++ jungle-garden/js/Flower.js
@@ -0,0 +1,80 @@
+class Flower {
+    // The constructor() sets up a flower's properties
+    constructor(x, y, size, stemLength, petalColor) {
+        // Position and size information
+        this.x = x;
+        this.y = y;
+        this.size = size;
+        this.stemLength = stemLength;
+        this.stemThickness = 10;
+        this.petalThickness = 10;
+        // Color information
+        this.stemColor = {
+            r: 50,
+            g: 150,
+            b: 50
+        };
+        this.petalColor = petalColor;
+        this.centreColor = {
+            r: 50,
+            g: 0,
+            b: 0
+        };
+        this.alive = true;
+    }
+
+    // display()
+    // Displays the flower on the canvas
+    display() {
+        push();
+        // Set the stroke weight for the petals and the stem
+        strokeWeight(this.stemThickness);
+        // Draw a line for the stem
+        stroke(this.stemColor.r, this.stemColor.g, this.stemColor.b);
+        line(this.x, this.y, this.x, this.y + this.stemLength);
+        // Draw a circle with a heavy outline for the flower
+        strokeWeight(this.petalThickness);
+        fill(this.centreColor.r, this.centreColor.g, this.centreColor.b);
+        stroke(this.petalColor.r, this.petalColor.g, this.petalColor.b);
+        ellipse(this.x, this.y, this.size);
+        pop();
+
+        this.x = constrain(this.x, 0, 500); //to keep the flowers on screen so that user can water them
+        this.y = constrain(this.y, 0, 500);
+    }
+
+    shrink() {
+        //random number to shrink by
+        let shrinkage = random(0, 0.1);
+        // make the petals smaller 
+        this.petalThickness = this.petalThickness - shrinkage / 10;
+        // make the center also shrink
+        this.size = this.size - shrinkage;
+
+        // If any of the key properties reach 0 or less, the flower is dead
+        if (this.petalThickness <= 0 || this.size <= 0) {
+            this.alive = false;
+        }
+    }
+
+
+
+    mousePressed() {
+        // Calculate the distance between this flower and the mouse
+        let d = dist(this.x, this.y, mouseX, mouseY);
+        // Check if the distance is less than the head of the flower
+        if (d < this.size / 2 + this.petalThickness) {
+            // If it is, this flower was clicked, so increase its stem length
+            this.stemLength = this.stemLength + 5;
+            // And also change its y position so it grows upward! (If we didn't do this
+            // the then stem would grow downward, which would look weird.)
+            this.y = this.y - 5;
+            //making the petals grow also
+            this.petalThickness = this.petalThickness + 0.5;
+            //making the center grow upon mouse click
+            this.size = this.size + 1;
+
+        }
+    }
+}
+
--- jungle-garden/js/script.js
+++ jungle-garden/js/script.js
@@ -1,82 +1,97 @@
 /**
- * Title of Project
- * Author Name
+ * Jungle Garden
+ * Arianna
  * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
+ * 
  */
 
 "use strict";
 
-/**
- * Description of preload
-*/
+let imgKirby;
+let imgButterfly;
 
-let backgroundColour = ("pink");
-
+// Our garden
+let garden = {
+    // An array to store the individual flowers
+    flowers: [],
+    // How many flowers in the garden
+    numFlowers: 5,
+    // Array for the amoung of butterflies
+    butterflys: [],
+    //amount of butterflies that will pop up 
+    numButters: 10,
+    // The color of the grass (background)
+    grassColor: {
+        r: 120,
+        g: 180,
+        b: 120
+    }
+};
 
 function preload() {
-
+    imgKirby = loadImage('assets/images/waterKirby.webp'); //kirby's image
+    imgButterfly = loadImage('assets/images/butterflypicture.png');
 }
 
-
-/**
- * Description of setup
-*/
+// setup() creates the canvas and the flowers in the garden
 function setup() {
-    createCanvas(500,500);
+    createCanvas(600, 600);
+
+    // Create our flowers by counting up to the number of the flowers
+    for (let i = 0; i < garden.numFlowers; i++) {
+        // Create variables for our arguments for clarity
+        let x = random(0, width);
+        let y = random(0, height);
+        let size = random(50, 80);
+        let stemLength = random(50, 100);
+        let petalColor = {
+            r: random(100, 255),
+            g: random(100, 255),
+            b: random(100, 255)
+        }
+        // Create a new flower using the arguments
+        let flower = new Flower(x, y, size, stemLength, petalColor);
+        // Add the flower to the array of flowers
+        garden.flowers.push(flower);
+    }
+    for (let i = 0; i < garden.numButters; i++) {
+        // Create a new butterfly using the arguments
+        let butter = new Butterfly(imgButterfly);
+        // Add the butterfly to the array of butterflys
+        garden.butterflys.push(butter);
+    }
 }
 
-function circleBig(x,y) {
-    push();
-    fill(mouseX,191,250);
-    noStroke();
-    translate(x,y);
-    circle(0,0,450)
-    pop();
-}
 
-
-function circleSmall(x,y) {
-
-   // let circleHeight = constrain(450,440,440);
-   // let circleWidth = constrain(450,440,440);
-
-    push();
-    fill(40,69,199);
-    strokeWeight(10);
-    translate(x,y);
-    point(0,0)
-    pop();
-}
-
-/**
- * Description of draw()
-*/
-
-
-
+// Displays our flowers
 function draw() {
-
-    background(backgroundColour);
-
-
-    let circleStartX = 20;
-    let circleStartY = 470;
-
-
-    circleBig(250,250);
-    let weird = constrain(mouseX,circleStartX,circleStartY);
-    let weird2 = constrain(mouseY,circleStartX,circleStartY);
+    // Display the grass
+    background(garden.grassColor.r, garden.grassColor.g, garden.grassColor.b);
     
 
-    
-    // 20/30    40/80   50/50   40/41  23/50
-    console.log(mouseX,"mouseX")
-    console.log(map(mouseX,0,500,0,255));
-   // console.log(mouseX*255/500)
+    // Loop through all the flowers in the array and display them
+    for (let i = 0; i < garden.flowers.length; i++) {
+        let flower = garden.flowers[i];
+        if (flower.alive) {
+            flower.shrink();
+            flower.display();
+        }
+    }
+    for (let i = 0; i < garden.butterflys.length; i++) {
+        let butter = garden.butterflys[i];
+        butter.move();
+        butter.draw();
+    }
+    let kirb = new Kirby(imgKirby);
+    kirb.draw();
+}
 
-
-    circleSmall(weird,weird2);
-
+function mousePressed() {
+    // Loop through every flower in the garden
+    for (let i = 0; i < garden.flowers.length; i++) {
+        // Get the current flower in the loop
+        let flower = garden.flowers[i];
+        // Call the flower's mousePressed() method
+        flower.mousePressed();
+    }
 }
