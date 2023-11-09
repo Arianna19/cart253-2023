e289c1040fdcdf3879714729f7ff3d914ccdbc29
Arianna <70153178+Arianna19@users.noreply.github.com>
2023-11-08 20:27:53 -0500

    Making bouncy balls from Pippin's code from the JungleGarden exercise as my base

---
+++ exercises/make-some-noise/js/Ball.js
@@ -0,0 +1,52 @@
+class Ball {
+
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+        this.vx = 0;
+        this.vy = 0;
+        this.ax = 0;
+        this.ay = 0;
+        this.maxSpeed = 10;
+        this.size = 40;
+        this.active = true;
+
+    }
+
+    //calling everything to be displayed and how it will be called in the main script
+    display() {
+        push();
+        fill(207, 37, 184);
+        noStroke();
+        ellipse(this.x, this.y, this.size);
+        pop();
+    }
+
+    //function in how the balls bascially jump up and down
+    move() {
+        this.vx = this.vx + this.ax;
+        this.vy = this.vy + this.ay;
+
+        this.vx = constrain(this.vx, -this.maxSpeed, this.maxSpeed);
+        this.vy = constrain(this.vy, -this.maxSpeed, this.maxSpeed);
+
+        this.x = this.x + this.vx;
+        this.y = this.y + this.vy;
+
+        //if (this.y - this.size / 2 > height) {
+         //   this.active = false;
+        //}
+    }
+
+    gravity(force) {
+        this.ay = this.ay + force;
+    }
+
+    bounce() {
+        if (this.y + this.size/2 >= height) {
+            this.vy = -this.vy;
+            this.ay = 0;
+        }
+    }
+}
+
--- exercises/make-some-noise/js/script.js
+++ exercises/make-some-noise/js/script.js
@@ -1,6 +1,7 @@
 /**
- * Title of Project
- * Author Name
+ * Make Some Noise exercise
+ * 
+ *  Arianna Narita
  * 
  * This is a template. You must fill in the title, author, 
  * and this description to match your project!
@@ -8,25 +9,44 @@
 
 "use strict";
 
-/**
- * Description of preload
-*/
+let gravityForce = 0.0025;
+
+//array for the balls spawning
+let balls = [];
+//amount of balls to show in the array
+let numBalls = 3;
+
+
 function preload() {
 
 }
 
-
-/**
- * Description of setup
-*/
 function setup() {
+
+    createCanvas(windowWidth, windowHeight);
+
+    for (let i = 0; i < numBalls; i++) {
+        let x = random(0, width);
+        let y = random(-400, -100);
+        let ball = new Ball(x, y);
+        balls.push(ball);
+    }
 
 }
 
-
-/**
- * Description of draw()
-*/
 function draw() {
+
+    background(219, 191, 90);
+
+    for (let i = 0; i < balls.length; i++) {
+        let ball = balls[i];
+        //if (ball.active) {
+            ball.gravity(gravityForce);
+            ball.move();
+            ball.bounce();
+            ball.display();
+       // }
+    }
+
 
 }
