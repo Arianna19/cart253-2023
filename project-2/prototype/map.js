aabd9b933251f0cd9a0385796ea80258e1d98fd7
Arianna <70153178+Arianna19@users.noreply.github.com>
2023-11-18 19:31:59 -0500

    Creating more platforms and bigger world

--- project-2/prototype/map.js
+++ project-2/prototype/map.js
@@ -1,8 +1,12 @@
 class mainMap {
+    
     constructor(kirb){
-        this.mainFloorPlat = new Platform(-100, 800, 90, 1100, kirb);
-        this.plat2 = new Platform(250, 600, 50, 300, kirb);
-        this.plat3 = new Platform(100, 300, 50, 150, kirb)
+        //amount of 'mini game' platforms that will be in the main map (total 5)
+        this.mainFloorPlat = new Platform(-100, 800, 90, 3000, kirb);
+        this.plat2 = new Platform(500, 600, 50, 250, kirb);
+        this.plat3 = new Platform(100, 300, 50, 150, kirb); //in reality this is the first one
+        this.plat4 = new Platform(900, 400, 50, 360, kirb);
+        this.plat5 = new Platform(1300, 150, 50, 90, kirb);
 
 
     }
@@ -10,9 +14,15 @@ class mainMap {
 
 
 draw() {
+    //calling each platform and 'drawing them'
     this.mainFloorPlat.draw();
     this.plat2.draw();
     this.plat3.draw();
+    this.plat4.draw();
+    this.plat5.draw();
+
+    
+
 
 
 
